e81a1e56a1b52e332029e0b358ba87d588890136
zhenghejie <1170108607@qq.com>
2023-10-22 18:15:01 +0800

    添加leetcode第3题

---
+++ src/leetcode/Solution3.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.*;
+
+public class Solution3 {
+    public static void main(String[] args) {
+        //思路：map存储字符数据，通过左右指针，左指针减，右指针加
+        //定义flag，有重复flag=true，直到做指针遇到重复位flag=flag
+
+        lengthOfLongestSubstring("pwwkew");
+
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        int len = s.length();
+        if(len==0) return 0;
+        else if(len == 1) return 1;
+        int l = 0;
+        int r = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        int res = 0;
+        while (r<len){
+            Character temp = s.charAt(r);
+            map.put(temp,map.getOrDefault(temp,0)+1);
+            r++;
+            while (map.get(temp)>1 && l<=r && l<len){
+                Character lTemp = s.charAt(l);
+                map.put(lTemp,map.getOrDefault(lTemp,0)-1);
+                l++;
+            }
+            res = Math.max(res,r-l);
+        }
+        return res;
+    }
+}
